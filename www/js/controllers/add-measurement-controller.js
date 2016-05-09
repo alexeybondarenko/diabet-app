7c62921955c2593204640e0d66c7123908aa00e8
Alexey Bondarenko <alexeybondarenko@me.com>
2016-05-09 10:29:06 +0300

    add n3-charts

--- www/js/controllers/add-measurement-controller.js
+++ www/js/controllers/add-measurement-controller.js
@@ -1,12 +1,16 @@
 'use strict';
 
-angular.module('app').controller('AddMeasurementController', function ($scope, $state, Measurement, Measurements) {
+angular.module('app').controller('AddMeasurementController', function ($scope, $state, $log, Measurement, Measurements) {
 
+  $log.debug('new controller');
   var measurement = new Measurement();
   $scope.model = measurement;
 
   $scope.save = function (form) {
-    if (form.$invalid) return;
+    if (form.$invalid) {
+      $log.debug('invalid form');
+      return;
+    }
 
     Measurements.add(measurement);
     $state.go('dashboard')
